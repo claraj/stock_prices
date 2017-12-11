cdc14c8b35755be1a0b006226adf0f242bf4dd7d
Clara <minneapolis-edu@users.noreply.github.com>
2017-12-11 13:05:36 -0600

    Update StockDataQueryFailedException.java

--- app/src/main/java/com/clara/stockcharts/model/StockDataQueryFailedException.java
+++ app/src/main/java/com/clara/stockcharts/model/StockDataQueryFailedException.java
@@ -9,7 +9,7 @@ public class StockDataQueryFailedException extends Exception {
 	public static final String ERR_NOT_FOUND = "Stock symbol not found";
 	public static final String ERR_INVALID_STOCK_PARAMETER = "Stock symbol missing or invalid";
 	public static final String ERR_NO_CONNECTION = "Can't connect to stock price server";
-	public static final String ERR_BAD_RESONSE = "Error fetching data from stock price server";
+	public static final String ERR_BAD_RESPONSE = "Error fetching data from stock price server";
 
 
 	public StockDataQueryFailedException(String msg) {
